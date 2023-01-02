9f9db701ecc48b09c81f6a57ab0d0990b1724f1d
Sashin <sashinchetty8@gmail.com>
2023-01-02 12:32:28 +0200

    Added cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Shop from "./pages/shop/Shop";
 import ShopContextProvider from "./context/ShopContextProvider";
+import { Cart } from "./pages/cart/cart";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<Shop />} />
+					<Route path="/cart" element={<Cart />} />
 				</Routes>
 			</Router>
 		</ShopContextProvider>
